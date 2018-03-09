099421fce5f39d08c3ceb28759030157de5ea13c
MIGUEL SALEMA AMARAL <9170450@eseig.pt>
2018-03-09 16:44:14 +0000

    Ecercices 1 DONE AND exercice 2, a and b are done , a bunch more to go

--- Ficha 1/index.js
+++ Ficha 1/index.js
@@ -1,3 +1,19 @@
 function Greatings() {
     console.log("Hello World!")
 } 
+
+function HelloName() {
+     
+
+    // Cria uma função anónima
+    let f = function(name) {
+        console.log(name)
+    }
+
+    // Invoca a função
+    let n = "ric"  
+    f(n)
+
+
+
+}
