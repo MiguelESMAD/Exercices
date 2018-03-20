aabdf729ac0b318cf7e2ab69e94d47f4cf18a0e8
MIGUEL SALEMA AMARAL <9170450@eseig.pt>
2018-03-20 10:12:17 +0000

    these will be modifed and have the solutions already

---
+++ Ficha4/indexResol.js
@@ -0,0 +1,37 @@
+
+
+
+   let show =function(value){
+       let myTextArea = document.querySelector("#txResults")
+       myTextArea.innerHTML += value + "\n"
+   }
+
+   
+
+   let student = {
+    name: "David Silva",
+    course: "POO",
+    grade: 12,
+    toString: function() {
+        return this.name + this.course + "#" + this.grade
+    }
+   }
+
+
+
+   //funçao para listarn os nomes das propriadades do objecto
+
+   function funcA(){
+       let str =""
+       for(var key in student ){
+           str += key
+       }
+       show("A-> " + str)
+   }
+
+   //funçao para listarn os nomes das propriadades do objecto, remover 'propriedades e listar
+   function  funcB() {
+       funcA()
+       delete student
+
+   }
