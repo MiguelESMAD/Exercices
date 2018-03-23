1fbc02e4aac015117cf391e30c286e1cad079365
MIGUEL SALEMA AMARAL <9170450@eseig.pt>
2018-03-23 16:18:41 +0000

    having trouble whit  first exercice

---
+++ Ficha 2/index.js
@@ -0,0 +1,15 @@
+
+let arNUmb = [7, 3, 6, 12, 5];
+let arStr = ["Vermelho", "Verde", "Branco", "Preto"];
+
+function number_average(arNUmb) {
+    let total1=0
+    let numbs = arNUmb
+    for(let i=0; i < 4; i++){
+        total1= numbs[i]
+    }
+    let totalFinal= total1/5
+
+
+
+}
