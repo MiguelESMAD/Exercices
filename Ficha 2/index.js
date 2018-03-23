2731f8ede0d33b3c1fbb8df0eeef46212bc26baf
MIGUEL SALEMA AMARAL <9170450@eseig.pt>
2018-03-23 16:28:40 +0000

    still stucked

--- Ficha 2/index.js
+++ Ficha 2/index.js
@@ -6,10 +6,8 @@ function number_average(arNUmb) {
     let total1=0
     let numbs = arNUmb
     for(let i=0; i < 4; i++){
-        total1= numbs[i]
+        total1+= numbs[i]
     }
     let totalFinal= total1/5
-
-
 
 }
