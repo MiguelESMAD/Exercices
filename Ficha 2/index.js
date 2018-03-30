b0dba49c09f4cee6211d3f2dee8a09bab73ec84d
MiguelESMAD <mikefirebun@gmail.com>
2018-03-30 15:10:27 +0100

    starting the exercice 1 over again and there seems to be a n odd error

--- Ficha 2/index.js
+++ Ficha 2/index.js
@@ -1,6 +1,7 @@
 //-------------------------
-// solution help here: https://github.com/rqueiros/MyRep
+// solution help here: https://github.com/rqueiros/OOP_TSIW/tree/master/Ficha02
 
+// this part is for the my textarea so that it shows the results 
 
 let show = function(value) {
     let myTextArea = document.querySelector("#txtResults")
@@ -13,12 +14,5 @@ myTextArea.innerHTML += value + "\n"
 let arNUmb = [7, 3, 6, 12, 5];
 let arStr = ["Vermelho", "Verde", "Branco", "Preto"];
 
-function number_average(arNUmb) {
-    let total1=0
-    let numbs = arNUmb
-    for(let i=0; i < 4; i++){
-        total1+= numbs[i]
-    }
-    let totalFinal= total1/5
+function number_average(arNUmb) {}
 
-}
