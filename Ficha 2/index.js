e25626fe9c46a52556bb467f11cb330833daf45f
MiguelESMAD <mikefirebun@gmail.com>
2018-03-30 16:16:48 +0100

    Exercice a to c are complete and its going smoothly now

--- Ficha 2/index.js
+++ Ficha 2/index.js
@@ -6,6 +6,7 @@
 let show = function(value) {
     let myTextArea = document.querySelector("#txtResults")
 myTextArea.innerHTML += value + "\n"
+}
 
 
 
@@ -14,5 +15,53 @@ myTextArea.innerHTML += value + "\n"
 let arNUmb = [7, 3, 6, 12, 5];
 let arStr = ["Vermelho", "Verde", "Branco", "Preto"];
 
-function number_average(arNUmb) {}
+function number_average() {
+    let numbl= arNUmb.length 
+    let soma=0
+    for(let i=0; i<numbl; i++){
+        soma+= arNUmb[i]
+
+    }
+    let result= soma/numbl
+
+    show("A-> " + result)
+
+    console.log(soma)
+    console.log(result)
+
+    //putting console logs help you know if the variables you incert are doing there job
+
+}
+
+function number_lowest(){
+    let numbl= arNUmb.length
+    let lowest = arNUmb[0]
+
+    for(let i=0; i<numbl; i++){
+        if(lowest>arNUmb[i]){
+            lowest=arNUmb[i]
+            console.log(lowest)
+        }
+    }
+
+    show("B-> "+ lowest)
+
+}
+
+function number_remove(){
+    let numbl = arNUmb.length
+    let Nnumb = 12
+    let NewArray =[]
+    
+
+    for(let i= 0; i<numbl; i++){
+        if(arNUmb[i] != Nnumb){
+            NewArray.push(arNUmb[i])
+        }
+
+    }
+
+    show("C-> "+ NewArray)
+}
+
 
