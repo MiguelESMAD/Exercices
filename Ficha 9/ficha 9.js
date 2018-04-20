1f86f472480351168065e18c27f4a5afa8fa0485
MIGUEL SALEMA AMARAL <9170450@eseig.pt>
2018-04-20 15:52:15 +0100

    trying and exploring on my own,  because thats the only way

--- Ficha 9/ficha 9.js
+++ Ficha 9/ficha 9.js
@@ -5,4 +5,13 @@
 
 window.onload = function(){
     let frmRegister = document.getElementById("frmRegis")
+
+    
+}
+
+//this will
+window.onclick = function(event) {
+    if (event.target == frmRegister) {
+        frmRegister.style.display = "none";
+    }
 }
